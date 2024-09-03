f084bccbde06389adda9e6c6aac9897295ffc3b6
positoro <positoro@gmail.com>
2024-09-03 12:17:53 +0900

    04_5

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -4,5 +4,8 @@ fn main() {
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
     // let /* your pattern here */ = cat;
 
+    let name: &str = cat.0;
+    let age: f32 = cat.1;
+
     println!("{name} is {age} years old");
 }
